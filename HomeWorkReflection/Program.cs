8c24571a8dff6646961a2a8a22b0a0dbabe72b7e
ygrek <ygreksh@gmail.com>
2021-08-20 13:21:24 +0300

    method invoke

---
+++ HomeWorkReflection/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace HomeWorkReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.LoadFrom(@"c:\Users\ygrek\RiderProjects\FiguresSerialize\FiguresSerialize\FiguresSerialize\bin\Debug\net5.0\FiguresSerialize.dll");
+            Console.WriteLine(assembly.FullName);
+            var myType = assembly.GetType("FiguresSerialize.Figure", false, true);
+            Object[] arguments = new object[] { "triangle", 3, 2 }; 
+            Object obj = Activator.CreateInstance(myType, arguments);
+            MethodInfo method = myType.GetMethod("Display");
+            method.Invoke(obj,null);
+        }
+    }
+}
