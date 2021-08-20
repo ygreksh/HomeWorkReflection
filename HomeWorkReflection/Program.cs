43d98e8f138e7c25ef92b0df1fd4611fab72ac31
ygrek <ygreksh@gmail.com>
2021-08-20 14:20:14 +0300

    Set value for closed property

--- HomeWorkReflection/Program.cs
+++ HomeWorkReflection/Program.cs
@@ -7,13 +7,33 @@ namespace HomeWorkReflection
     {
         static void Main(string[] args)
         {
-            Assembly assembly = Assembly.LoadFrom(@"c:\Users\ygrek\RiderProjects\FiguresSerialize\FiguresSerialize\FiguresSerialize\bin\Debug\net5.0\FiguresSerialize.dll");
-            Console.WriteLine(assembly.FullName);
-            var myType = assembly.GetType("FiguresSerialize.Figure", false, true);
-            Object[] arguments = new object[] { "triangle", 3, 2 }; 
-            Object obj = Activator.CreateInstance(myType, arguments);
-            MethodInfo method = myType.GetMethod("Display");
-            method.Invoke(obj,null);
+            try
+            {
+                Assembly assembly = Assembly.LoadFrom(@"c:\Users\ygrek\RiderProjects\FiguresSerialize\FiguresSerialize\FiguresSerialize\bin\Debug\net5.0\FiguresSerialize.dll");
+                Console.WriteLine(assembly.FullName);
+                var myType = assembly.GetType("FiguresSerialize.Figure", false, true);
+                Object[] arguments = new object[] { "triangle", 3, 2 }; 
+                Object obj = Activator.CreateInstance(myType, arguments);
+                MethodInfo method = myType.GetMethod("Display");
+                method.Invoke(obj,null);
+                PropertyInfo closedProperty = myType.GetProperty("closedProperty",BindingFlags.Instance | BindingFlags.NonPublic);
+                
+                if (closedProperty !=null)
+                {
+                    Console.WriteLine($"Closed Property: {closedProperty.Name}");
+                }
+                else
+                {
+                    Console.WriteLine("Closed property not found!");
+                }
+                closedProperty.SetValue(obj, "Set by Reflection");
+                method.Invoke(obj,null);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
